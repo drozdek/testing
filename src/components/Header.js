b40f8b89274d16c00525e8a758ab3d8b10e1eee7
rafal <raf.drozdowski@gmail.com>
2020-02-02 17:36:58 +0000

    add Header components / add test for Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Companies header and logo </h2>
+    </header>
+  )
+};
+
+export default Header;
