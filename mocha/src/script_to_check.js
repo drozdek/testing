e7f295f75d02c0582ee6a814b631b5efba465acc
Rafal <drozdek@users.noreply.github.com>
2020-12-23 20:12:44 +0000

    adds script to be tested

---
+++ mocha/src/script_to_check.js
@@ -0,0 +1,21 @@
+function pow(i, x) {
+    return Math.pow(i, x);
+};
+
+
+function checkNumber(n) {
+    var isNo = Number.isInteger(n) ? 1 : false;
+    return isNo;
+};
+
+
+var arrayToCheck = [1, 2, 3, 4, 5, 6, 7];
+
+
+class MyClass {
+    constructor() {
+        name: "Rafal"
+    };
+};
+var mc = new MyClass();
+
