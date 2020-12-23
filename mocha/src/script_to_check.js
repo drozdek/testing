e79ec7ea1c7c09ef81924d8c7466d0e3aa70b77f
Rafal <drozdek@users.noreply.github.com>
2020-12-23 20:15:14 +0000

    reformats code

--- mocha/src/script_to_check.js
+++ mocha/src/script_to_check.js
@@ -2,20 +2,18 @@ function pow(i, x) {
     return Math.pow(i, x);
 };
 
-
 function checkNumber(n) {
     var isNo = Number.isInteger(n) ? 1 : false;
     return isNo;
 };
 
-
 var arrayToCheck = [1, 2, 3, 4, 5, 6, 7];
-
 
 class MyClass {
     constructor() {
         name: "Rafal"
     };
 };
+
 var mc = new MyClass();
 
