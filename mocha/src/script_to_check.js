3f0e0a823d79218767c840d3453ca212638b7fa4
Rafal <raf.drozdowski@gmail.com>
2020-12-29 21:57:08 +0000

    adds listing to tests adds add function

--- mocha/src/script_to_check.js
+++ mocha/src/script_to_check.js
@@ -1,14 +1,23 @@
+// 1.
 function pow(i, x) {
     return Math.pow(i, x);
 };
 
+// 2.
+function add(i, x){
+    return i + x
+}
+
+// 3.
 function checkNumber(n) {
     var isNo = Number.isInteger(n) ? 1 : false;
     return isNo;
 };
 
+// 4.
 var arrayToCheck = [1, 2, 3, 4, 5, 6, 7];
 
+// 5.
 class MyClass {
     constructor() {
         name: "Rafal"
